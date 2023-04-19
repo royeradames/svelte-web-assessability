eda51dc9170c7646644db3921850a15fe32c6511
Royer Adames <royeraadames@gmail.com>
2023-04-19 18:38:17 -0400

    reset the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,231 +1,231 @@
- <!-- MAIN CONTENT -->
-    <div class='main'>
-      <section class='spotlight-container'>
-        <ul class='spotlight-list'>
-          <li class='spotlight-item'>
-            <div class='spotlight-item-inner'>
-              <a class='spotlight-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/introducing-java-for-programmers-1.png'/>
-              </a>
-              <div class='post-info'>
-                <span class='post-tag'>learning to code</span>
-                <h2 class='post-title primary-text-color'>
-                  <a>Introducing Java for Programmers</a>
-                </h2>
-                <div class='post-meta'>
+<!-- MAIN CONTENT -->
+<div class='main'>
+  <section class='spotlight-container'>
+    <ul class='spotlight-list'>
+      <li class='spotlight-item'>
+        <div class='spotlight-item-inner'>
+          <a class='spotlight-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/introducing-java-for-programmers-1.png'/>
+          </a>
+          <div class='post-info'>
+            <span class='post-tag'>learning to code</span>
+            <h2 class='post-title primary-text-color'>
+              <a>Introducing Java for Programmers</a>
+            </h2>
+            <div class='post-meta'>
                   <span class='post-author primary-text-color'>
                     <a>Codecademy</a>
                   </span>
-                  <span class='post-date secondary-text-color'>February 4, 2021</span>
-                </div>
-              </div>
+              <span class='post-date secondary-text-color'>February 4, 2021</span>
             </div>
-          </li>
-          <li class='spotlight-item-sm'>
-            <div class='content-item-image-container'>
-              <span class='post-tag'>learning to code</span>
-              <a class='content-item-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/introducing-java-for-programmers-1.png'/>
-              </a>
-            </div>
-            <div class='content-item-info'>
-              <h2 class='primary-text-color'>
-                <a>Introducing Java for Programmers</a>
-              </h2>
-              <div class='post-meta'>
+          </div>
+        </div>
+      </li>
+      <li class='spotlight-item-sm'>
+        <div class='content-item-image-container'>
+          <span class='post-tag'>learning to code</span>
+          <a class='content-item-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/introducing-java-for-programmers-1.png'/>
+          </a>
+        </div>
+        <div class='content-item-info'>
+          <h2 class='primary-text-color'>
+            <a>Introducing Java for Programmers</a>
+          </h2>
+          <div class='post-meta'>
                 <span class='post-author primary-text-color'>
                   <a>Codecademy</a>
                 </span>
-                <span class='post-date secondary-text-color'>February 4, 2021</span>
-              </div>
-              <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
-              <a href='#'>
-                <button class='btn read-more'>Read More</button>
-              </a>
-            </div>
-          </li>
-          <li class='spotlight-item'>
-            <div class='spotlight-item-inner'>
-              <a class='spotlight-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/code-mentor.png'/>
-              </a>
-              <div class='post-info'>
-                <span class='post-tag'>learning to code</span>
-                <h2 class='post-title primary-text-color'>
-                  <a>3 Ways a Coding Mentor Can Help You Learn</a>
-                </h2>
-                <div class='post-meta'>
+            <span class='post-date secondary-text-color'>February 4, 2021</span>
+          </div>
+          <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
+          <a href='#'>
+            <button class='btn read-more'>Read More</button>
+          </a>
+        </div>
+      </li>
+      <li class='spotlight-item'>
+        <div class='spotlight-item-inner'>
+          <a class='spotlight-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/code-mentor.png'/>
+          </a>
+          <div class='post-info'>
+            <span class='post-tag'>learning to code</span>
+            <h2 class='post-title primary-text-color'>
+              <a>3 Ways a Coding Mentor Can Help You Learn</a>
+            </h2>
+            <div class='post-meta'>
                   <span class='post-author primary-text-color'>
                     <a>Codecademy</a>
                   </span>
-                  <span class='post-date secondary-text-color'>April 2, 2021</span>
-                </div>
-              </div>
+              <span class='post-date secondary-text-color'>April 2, 2021</span>
             </div>
-          </li>
-          <li class='spotlight-item-sm'>
-            <div class='content-item-image-container'>
-              <span class='post-tag'>learning to code</span>
-              <a class='content-item-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/code-mentor.png'/>
-              </a>
-            </div>
-            <div class='content-item-info'>
-              <h2 class='primary-text-color'>
-                <a>3 Ways a Coding Mentor Can Help You Learn</a>
-              </h2>
-              <div class='post-meta'>
+          </div>
+        </div>
+      </li>
+      <li class='spotlight-item-sm'>
+        <div class='content-item-image-container'>
+          <span class='post-tag'>learning to code</span>
+          <a class='content-item-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/code-mentor.png'/>
+          </a>
+        </div>
+        <div class='content-item-info'>
+          <h2 class='primary-text-color'>
+            <a>3 Ways a Coding Mentor Can Help You Learn</a>
+          </h2>
+          <div class='post-meta'>
                 <span class='post-author primary-text-color'>
                   <a>Codecademy</a>
                 </span>
-                <span class='post-date secondary-text-color'>April 2, 2021</span>
-              </div>
-              <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
-              <a href='#'>
-                <button class='btn read-more'>Read More</button>
-              </a>
-            </div>
-          </li>
-          <li class='spotlight-item'>
-            <div class='spotlight-item-inner'>
-              <a class='spotlight-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/programming-languages.png'/>
-              </a>
-              <div class='post-info'>
-                <span class='post-tag'>learning to code</span>
-                <h2 class='post-title primary-text-color'>
-                  <a>What is a Programming Language?</a>
-                </h2>
-                <div class='post-meta'>
+            <span class='post-date secondary-text-color'>April 2, 2021</span>
+          </div>
+          <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
+          <a href='#'>
+            <button class='btn read-more'>Read More</button>
+          </a>
+        </div>
+      </li>
+      <li class='spotlight-item'>
+        <div class='spotlight-item-inner'>
+          <a class='spotlight-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/programming-languages.png'/>
+          </a>
+          <div class='post-info'>
+            <span class='post-tag'>learning to code</span>
+            <h2 class='post-title primary-text-color'>
+              <a>What is a Programming Language?</a>
+            </h2>
+            <div class='post-meta'>
                   <span class='post-author primary-text-color'>
                     <a>Codecademy</a>
                   </span>
-                  <span class='post-date secondary-text-color'>July 22, 2020</span>
-                </div>
-              </div>
+              <span class='post-date secondary-text-color'>July 22, 2020</span>
             </div>
-          </li>
-          <li class='spotlight-item-sm'>
-            <div class='content-item-image-container'>
-              <span class='post-tag'>learning to code</span>
-              <a class='content-item-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/programming-languages.png'/>
-              </a>
-            </div>
-            <div class='content-item-info'>
-              <h2 class='primary-text-color'>
-                <a>What is a Programming Language?</a>
-              </h2>
-              <div class='post-meta'>
+          </div>
+        </div>
+      </li>
+      <li class='spotlight-item-sm'>
+        <div class='content-item-image-container'>
+          <span class='post-tag'>learning to code</span>
+          <a class='content-item-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/programming-languages.png'/>
+          </a>
+        </div>
+        <div class='content-item-info'>
+          <h2 class='primary-text-color'>
+            <a>What is a Programming Language?</a>
+          </h2>
+          <div class='post-meta'>
                 <span class='post-author primary-text-color'>
                   <a>Codecademy</a>
                 </span>
-                <span class='post-date secondary-text-color'>July 22, 2020</span>
-              </div>
-              <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
-              <a href='#'>
-                <button class='btn read-more'>Read More</button>
-              </a>
-            </div>
-          </li>
-        </ul>
-      </section>
-      <section class='main-content'>
-        <ul class='content-list'>
-          <li class='content-list-item'>
-            <div class='content-item-image-container'>
-              <span class='post-tag'>learning to code</span>
-              <a class='content-item-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/build-ios-apps-with-swiftui-1024x529.png'/>
-              </a>
-            </div>
-            <div class='content-item-info'>
-              <h2 class='primary-text-color'>
-                <!-- Add <abbr> tag for Apps -->
-                <a>Introducing Build iOS Apps with SwiftUI</a>
-              </h2>
-              <div class='post-meta'>
+            <span class='post-date secondary-text-color'>July 22, 2020</span>
+          </div>
+          <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
+          <a href='#'>
+            <button class='btn read-more'>Read More</button>
+          </a>
+        </div>
+      </li>
+    </ul>
+  </section>
+  <section class='main-content'>
+    <ul class='content-list'>
+      <li class='content-list-item'>
+        <div class='content-item-image-container'>
+          <span class='post-tag'>learning to code</span>
+          <a class='content-item-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/build-ios-apps-with-swiftui-1024x529.png'/>
+          </a>
+        </div>
+        <div class='content-item-info'>
+          <h2 class='primary-text-color'>
+            <!-- Add <abbr> tag for Apps -->
+            <a>Introducing Build iOS Apps with SwiftUI</a>
+          </h2>
+          <div class='post-meta'>
                 <span class='post-author primary-text-color'>
                   <a>Codecademy</a>
                 </span>
-                <span class='post-date secondary-text-color'>July 22, 2020</span>
-              </div>
-              <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
-              <a href='#'>
-                <button class='btn read-more'>Read More</button>
-              </a>
-            </div>
-          </li>
-          <li class='content-list-item'>
-            <div class='content-item-image-container'>
-              <span class='post-tag'>learning to code</span>
-              <a class='content-item-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/How-to-get-a-job-as-a-developer-1024x476.png'/>
-              </a>
-            </div>
-            <div class='content-item-info'>
-              <h2 class='primary-text-color'>
-                <a>How to Get a Job as a Developer</a>
-              </h2>
-              <div class='post-meta'>
+            <span class='post-date secondary-text-color'>July 22, 2020</span>
+          </div>
+          <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
+          <a href='#'>
+            <button class='btn read-more'>Read More</button>
+          </a>
+        </div>
+      </li>
+      <li class='content-list-item'>
+        <div class='content-item-image-container'>
+          <span class='post-tag'>learning to code</span>
+          <a class='content-item-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/How-to-get-a-job-as-a-developer-1024x476.png'/>
+          </a>
+        </div>
+        <div class='content-item-info'>
+          <h2 class='primary-text-color'>
+            <a>How to Get a Job as a Developer</a>
+          </h2>
+          <div class='post-meta'>
                 <span class='post-author primary-text-color'>
                   <a>Codecademy</a>
                 </span>
-                <span class='post-date secondary-text-color'>July 22, 2020</span>
-              </div>
-              <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
-              <a href='#'>
-                <button class='btn read-more'>Read More</button>
-              </a>
-            </div>
-          </li>
-          <li class='content-list-item'>
-            <div class='content-item-image-container'>
-              <span class='post-tag'>learning to code</span>
-              <a class='content-item-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/what-is-open-source-1-1024x476.png'/>
-              </a>
-            </div>
-            <div class='content-item-info'>
-              <h2 class='primary-text-color'>
-                <a>Understanding Open Source Software: Q & A</a>
-              </h2>
-              <div class='post-meta'>
+            <span class='post-date secondary-text-color'>July 22, 2020</span>
+          </div>
+          <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
+          <a href='#'>
+            <button class='btn read-more'>Read More</button>
+          </a>
+        </div>
+      </li>
+      <li class='content-list-item'>
+        <div class='content-item-image-container'>
+          <span class='post-tag'>learning to code</span>
+          <a class='content-item-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/what-is-open-source-1-1024x476.png'/>
+          </a>
+        </div>
+        <div class='content-item-info'>
+          <h2 class='primary-text-color'>
+            <a>Understanding Open Source Software: Q & A</a>
+          </h2>
+          <div class='post-meta'>
                 <span class='post-author primary-text-color'>
                   <a>Codecademy</a>
                 </span>
-                <span class='post-date secondary-text-color'>July 22, 2020</span>
-              </div>
-              <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
-              <a href='#'>
-                <button class='btn read-more'>Read More</button>
-              </a>
-            </div>
-          </li>
-          <li class='content-list-item'>
-            <div class='content-item-image-container'>
-              <span class='post-tag'>learning to code</span>
-              <a class='content-item-image-link' href='#'>
-                <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/top-CSS-interview-questions.png'/>
-              </a>
-            </div>
-            <div class='content-item-info'>
-              <h2 class='primary-text-color'>
-                <!-- Add <abbr> tag for CSS -->
-                <a>7 Top <abbr title="Cascading Style Sheets">CSS</abbr> Interview Questions to Practice</a>
-              </h2>
-              <div class='post-meta'>
+            <span class='post-date secondary-text-color'>July 22, 2020</span>
+          </div>
+          <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
+          <a href='#'>
+            <button class='btn read-more'>Read More</button>
+          </a>
+        </div>
+      </li>
+      <li class='content-list-item'>
+        <div class='content-item-image-container'>
+          <span class='post-tag'>learning to code</span>
+          <a class='content-item-image-link' href='#'>
+            <img src='https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/top-CSS-interview-questions.png'/>
+          </a>
+        </div>
+        <div class='content-item-info'>
+          <h2 class='primary-text-color'>
+            <!-- Add <abbr> tag for CSS -->
+            <a>7 Top CSS Interview Questions to Practice</a>
+          </h2>
+          <div class='post-meta'>
                 <span class='post-author primary-text-color'>
                   <a>Codecademy</a>
                 </span>
-                <span class='post-date secondary-text-color'>July 22, 2020</span>
-              </div>
-              <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
-              <a href='#'>
-                <button class='btn read-more'>Read More</button>
-              </a>
-            </div>
-          </li>
-        </ul>
-      </section>
-    </div>
+            <span class='post-date secondary-text-color'>July 22, 2020</span>
+          </div>
+          <p class='summary primary-text-color'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th…</p>
+          <a href='#'>
+            <button class='btn read-more'>Read More</button>
+          </a>
+        </div>
+      </li>
+    </ul>
+  </section>
+</div>
